7d6739d6159cd169335bd25f10db333a124ce4b3
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-08-14 11:47:28 -0400

    add check phase back

--- gaia/default.nix
+++ gaia/default.nix
@@ -4,9 +4,9 @@ pkgs.buildGoApplication {
   src = "${gaia-src}";
   modules = ./go-modules.toml;
 
-  # NOTE: we need to disable the check phase since the tests use $HOME which
-  # is not allowed in nix's hermetic environment.
-  # N.B. $HOME = /homeless-shelter with no write permissions
-  doCheck = false;
+  # NOTE: we need to create a tmp home directory for gaia's tests
+  preCheck = ''
+    export HOME="$(mktemp -d)"
+  '';
 }
 
