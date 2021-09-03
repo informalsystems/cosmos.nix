38756a9e5d1d43e1e285cdda2809f85aa79d2f0f
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-03 13:45:57 -0400

    remove doCheck

--- stoml/default.nix
+++ stoml/default.nix
@@ -3,9 +3,5 @@ pkgs.buildGoApplication {
   name = "stoml";
   src = "${stoml-src}";
   modules = ./go-modules.toml;
-
-  # The tests for cosmovisor require a network connection
-  # and nix doesn't allow this, so we have to disable checks.
-  # doCheck = false;
 }
 
