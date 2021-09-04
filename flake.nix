6c02bebe32b8fc3240344fd4aabe1dee5e4d6012
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-04 17:58:20 -0400

    package gaia manager

--- flake.nix
+++ flake.nix
@@ -172,7 +172,9 @@
           '';
         in
         pkgs.mkShell {
-          shellHook = self.checks.${system}.pre-commit-check.shellHook;
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+          '';
           nativeBuildInputs = with pkgs; [
             rustc
             cargo
