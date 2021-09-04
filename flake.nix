fd90ffd82eb7062a4a787e81f5a1d27eec8557c6
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-04 18:29:53 -0400

    fix CI

--- flake.nix
+++ flake.nix
@@ -181,9 +181,7 @@
           '';
         in
         pkgs.mkShell {
-          shellHook = ''
-            ${self.checks.${system}.pre-commit-check.shellHook}
-          '';
+          shellHook = self.checks.${system}.pre-commit-check.shellHook;
           nativeBuildInputs = with pkgs; [
             rustc
             cargo
