a238e92ad34a0e55be58090c0b590b273ff17650
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-11 18:32:58 -0400

    enable check

--- gm/default.nix
+++ gm/default.nix
@@ -3,11 +3,11 @@ pkgs.stdenv.mkDerivation {
   name = "gm";
   unpackPhase = "true";
   buildPhase = "true";
-  # checkPhase = ''
-  #   ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/gm
-  #   ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/lib-gm
-  #   ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/shell-support
-  # '';
+  checkPhase = ''
+    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/gm
+    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/lib-gm
+    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/shell-support
+  '';
   installPhase = ''
     mkdir -p $out/bin
 
