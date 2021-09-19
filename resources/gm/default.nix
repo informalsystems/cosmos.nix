3efa4c69f9b27ff5d520e319d40fbcf7d110c091
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-19 15:05:06 -0400

    use patched src

--- resources/gm/default.nix
+++ resources/gm/default.nix
@@ -1,18 +1,29 @@
-{ pkgs, ibc-rs-src }:
-pkgs.stdenv.mkDerivation {
-  name = "gm";
-  unpackPhase = "true";
-  buildPhase = "true";
-  checkPhase = ''
-    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/gm
-    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/lib-gm
-    ${pkgs.shellcheck} ${ibc-rs-src}/scripts/gm/bin/shell-support
-  '';
-  installPhase = ''
+{ shellcheck, makeWrapper, mkDerivation, lib, ibc-rs-src, stoml, sconfig }:
+mkDerivation {
+  pname = "gm";
+  version = "0.7.0";
+  buildInputs = [ makeWrapper ];
+  src = ibc-rs-src;
+  configurePhase = "true";
+  buildPhase = ''
+    echo "installing gm from sources"
     mkdir -p $out/bin
 
-    ln -s ${ibc-rs-src}/scripts/gm/bin/gm $out/bin/gm
-    ln -s ${ibc-rs-src}/scripts/gm/bin/lib-gm $out/bin/lib-gm
-    ln -s ${ibc-rs-src}/scripts/gm/bin/shell-support $out/bin/shell-support
+    cp ./scripts/gm/bin/gm $out/bin/gm
+    cp ./scripts/gm/bin/lib-gm $out/bin/lib-gm
+    cp ./scripts/gm/bin/shell-support $out/bin/shell-support
   '';
+  checkPhase = ''
+    echo "checking bash scripts"
+    ${shellcheck}/bin/shellcheck ./bin/gm
+    ${shellcheck}/bin/shellcheck ./bin/lib-gm
+    ${shellcheck}/bin/shellcheck ./bin/shell-support
+  '';
+  installPhase = ''
+    echo "wrapping absolute paths to sconfig and stoml"
+    wrapProgram $out/bin/gm \
+      --prefix PATH : ${lib.makeBinPath [ sconfig stoml ]} \
+      --set LIB_GM_PATH $out/bin
+  '';
+  patches = [ ./gm_lib.patch ];
 }
