22443d4381ed3456c5601e71d9d7415fdda0fc48
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-09-28 14:24:31 -0400

    package ts-relayer

---
+++ ts-relayer/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ts-relayer-src }:
+let
+  ibc-relayer =
+    pkgs.mkYarnPackage {
+      name = "ts-relayer";
+      src = ts-relayer-src;
+      packageJSON = "${ts-relayer-src}/package.json";
+      postConfigure = ''
+        ${pkgs.yarn}/bin/yarn build
+      '';
+      yarnLock = "${ts-relayer-src}/yarn.lock";
+    };
+in
+{
+  ts-relayer = pkgs.writeShellScriptBin "ts-relayer" ''
+    ${pkgs.nodejs}/bin/node ${ibc-relayer}/bin/ibc-relayer
+  '';
+  ts-relayer-setup = pkgs.writeShellScriptBin "ts-relayer-setup" ''
+    ${pkgs.nodejs}/bin/node ${ibc-relayer}/bin/ibc-setup
+  '';
+}
+
