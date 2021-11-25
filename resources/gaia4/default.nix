4998d09648b67bf86bffe44e7fa197be0694aa34
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-11-24 19:52:00 -0500

    add ld flags to gaia 4 and 5

--- resources/gaia4/default.nix
+++ resources/gaia4/default.nix
@@ -1,11 +1,23 @@
 { pkgs, gaia4-src }:
-pkgs.buildGoApplication {
+let
   pname = "gaia";
   version = "4.2.1";
+  tendermint-version = (fromTOML (builtins.readFile ./go-modules.toml))."github.com/tendermint/tendermint".sumVersion;
+in
+pkgs.buildGoApplication {
+  inherit pname version;
   src = "${gaia4-src}";
   modules = ./go-modules.toml;
   preCheck = ''
     export HOME="$(mktemp -d)"
+  '';
+  buildFlagsArray = ''
+    -ldflags=
+    -X github.com/cosmos/cosmos-sdk/version.Name=${pname}
+    -X github.com/cosmos/cosmos-sdk/version.AppName=gaiad
+    -X github.com/cosmos/cosmos-sdk/version.Version=${version}
+    -X github.com/cosmos/cosmos-sdk/version.Commit=${gaia4-src.rev}
+    -X github.com/tendermint/tendermint/version.TMCoreSemVer=${tendermint-version}
   '';
 }
 
--- resources/gaia5/default.nix
+++ resources/gaia5/default.nix
@@ -1,11 +1,23 @@
 { pkgs, gaia5-src }:
-pkgs.buildGoApplication {
+let
   pname = "gaia";
   version = "5.0.6";
+  tendermint-version = (fromTOML (builtins.readFile ./go-modules.toml))."github.com/tendermint/tendermint".sumVersion;
+in
+pkgs.buildGoApplication {
+  inherit pname version;
   src = "${gaia5-src}";
   modules = ./go-modules.toml;
   preCheck = ''
     export HOME="$(mktemp -d)"
+  '';
+  buildFlagsArray = ''
+    -ldflags=
+    -X github.com/cosmos/cosmos-sdk/version.Name=${pname}
+    -X github.com/cosmos/cosmos-sdk/version.AppName=gaiad
+    -X github.com/cosmos/cosmos-sdk/version.Version=${version}
+    -X github.com/cosmos/cosmos-sdk/version.Commit=${gaia5-src.rev}
+    -X github.com/tendermint/tendermint/version.TMCoreSemVer=${tendermint-version}
   '';
 }
 
