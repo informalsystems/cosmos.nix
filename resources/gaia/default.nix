585e444383ddab8bf2abc6a8a333a90b668953f4
Soares Chen <soares.chen@gmail.com>
2022-05-06 11:59:03 +0200

    Update Gaia and ibc-go-simapp (#84)
    * Update Gaia and ibc-go-simapp
    
    * Fix gaia6 references

--- resources/gaia/default.nix
+++ resources/gaia/default.nix
@@ -16,27 +16,11 @@ in
         tags = ["netgo"];
       };
 
-      gaia6_0_2 = {
-        name = "gaia";
-        vendorSha256 = "sha256-CNxWgIWf+8wB2CAUk+WadnIb3fi1UYftPea5sWtk/Rs=";
-        version = "v6.0.2";
-        src = gaia6_0_2-src;
-        tags = ["netgo"];
-      };
-
-      gaia6_0_3 = {
-        name = "gaia";
-        vendorSha256 = "sha256-cNQOv4wW98Vd08ieU3jgsvXoSDQQYZTkeTqUD2Cty58=";
-        version = "v6.0.3";
-        src = gaia6_0_3-src;
-        tags = ["netgo"];
-      };
-
-      gaia6_0_4 = {
+      gaia6 = {
         name = "gaia";
         vendorSha256 = "sha256-KeF3gO5sUJEXWqb6EVYBYXpVBfhvyXZ4f03l63wYTjE=";
         version = "v6.0.4";
-        src = gaia6_0_4-src;
+        src = gaia6-src;
         tags = ["netgo"];
       };
 
@@ -48,11 +32,9 @@ in
         tags = ["netgo"];
       };
 
-      gaia6 = gaia6_0_4;
-
       gaia7 = {
         name = "gaia";
-        vendorSha256 = "sha256-fGRLYkxZDowkuHcX26aRclLind0PRKkC64CQBVrnBr8=";
+        vendorSha256 = "sha256-OLo/pQNqYguQXeOyUgEMeghjiBq2Tp0JrOz52uy+e/A=";
         version = "v7.0.0";
         src = gaia7-src;
         tags = ["netgo"];
