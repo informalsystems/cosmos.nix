74e7257cd4305697857fa17e9b8a931ae4d553a7
Soares Chen <soares.chen@gmail.com>
2022-07-18 12:59:38 +0200

    Add back `ibc-go-ics29-simapp` (#85)
    * Add back ibc-go-ics29-simapp
    
    * Update ibc-go-main
    
    * Update ibc-go-main
    
    * Update ibc-go
    
    * Update ibc-go versions
    
    * Revert ibc-go-main latest which requires Go 1.18
    
    * Use Go 1.18 to build latest ibc-go
    
    * Fix ibc-go-v4 src
    
    * Remove ibc-go-ics29-simapp which is superceeded by ibc-go-v4-simapp

--- resources/ibc-go/default.nix
+++ resources/ibc-go/default.nix
@@ -11,24 +11,32 @@ in
       ibc-go-v2-simapp = {
         name = "simapp";
         src = ibc-go-v2-src;
-        version = "v6.0.0-v3";
+        version = "v2.0.0";
         vendorSha256 = "sha256-Af47uEEPCFsX1JiMiw3LprGDiVb/0HA0sMeuDdAVXu8=";
         tags = ["netgo"];
       };
 
       ibc-go-v3-simapp = {
         name = "simapp";
-        version = "v6.0.0-v3";
+        version = "v3.0.0";
         src = ibc-go-v3-src;
-        vendorSha256 = "sha256-W05fH/y7InNgY68aJLlm32c8DpAKFnO3ehH8CzzYdPI=";
+        vendorSha256 = "sha256-kefoBLr1pbyycUjel6rZ8VsqCLbPFY5hUHUVyO+Y2wc=";
+        tags = ["netgo"];
+      };
+
+      ibc-go-v4-simapp = {
+        name = "simapp";
+        version = "v4.0.0";
+        src = ibc-go-v4-src;
+        vendorSha256 = "sha256-7NJoasvGMUtJqZpqLDm6+aVrKQw3VYO/13udb8wKz5s=";
         tags = ["netgo"];
       };
 
       ibc-go-main-simapp = {
         name = "simapp";
-        version = "v7.0.0-main";
+        version = "v4.0.0-main";
         src = ibc-go-main-src;
-        vendorSha256 = "sha256-2RBZNUIZVdPPI63FzkmOOPSlYlFE+UjzMMnqKEjayNY=";
+        vendorSha256 = "sha256-7NJoasvGMUtJqZpqLDm6+aVrKQw3VYO/13udb8wKz5s=";
         tags = ["netgo"];
       };
     }
