cddb475fcc6fb98ffd3d7f2c26dcdb0cab2c23ba
Soares Chen <soares.chen@gmail.com>
2022-10-05 14:01:50 +0200

    Update Hermes, ibc-go, gaia, and osmosis (#101)
    * Update ibc-go, gaia, and osmosis
    
    * Update Hermes, fix builds
    
    * Fix Osmosis build
    
    * Fix Hermes
    
    * Fix ibc-go build

--- resources.nix
+++ resources.nix
@@ -97,13 +97,14 @@
 
       osmosis = utilities.mkCosmosGoApp {
         name = "osmosis";
-        version = "v10.0.1";
+        version = "v12.1.0";
         src = inputs.osmosis-src;
-        vendorSha256 = "sha256-3EmJXxHZD/JcTm/agobnINm7qS4iHfC7bw5FiogaXiE=";
+        vendorSha256 = "sha256-1dcG5yqLxH2X5oGVDhuqXUlno3i95SmEPT1aOYCrFuA=";
         tags = ["netgo"];
         preFixup = ''
           ${utilities.wasmdPreFixupPhase "osmosisd"}
-          ${utilities.wasmdPreFixupPhase "chain_init"}
+          ${utilities.wasmdPreFixupPhase "chain"}
+          ${utilities.wasmdPreFixupPhase "node"}
         '';
         buildInputs = [libwasmvm_1];
 
@@ -269,11 +270,11 @@
 
       # Rust resources
       hermes = pkgs.rustPlatform.buildRustPackage {
-        pname = "ibc-rs";
-        version = "v0.13.0-rc.0";
+        pname = "hermes";
+        version = "v1.0.0";
         src = inputs.ibc-rs-src;
         nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default];
-        cargoSha256 = "sha256-lIMnZQw46prUFHlAzCWPkKzSNi4F9D+1+aG1vt/5Bvo=";
+        cargoSha256 = "sha256-0GZN3xq/5FC/jYXGVDIOrha+sB+Gv/6nzlFvpCAYO3M=";
         doCheck = false;
       };
 
