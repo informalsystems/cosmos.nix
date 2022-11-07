9db0dcd17c13604277f19805cefc78ffbf48e134
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-11-06 19:58:38 -0500

    fix copypasta

--- modules/chains/osmosis.nix
+++ modules/chains/osmosis.nix
@@ -122,73 +122,67 @@ in
           echo "Symlinking osmosis v2 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v2/bin" && ! -L "/root/.osmosisd/cosmovisor/v2/bin" ]]
           then
-            ln -s ${cfg.packages.v2}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v2}/bin/osmosisd /root/.osmosisd/cosmovisor/v2/bin
           fi
 
           echo "Symlinking osmosis v3 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v3/bin" && ! -L "/root/.osmosisd/cosmovisor/v3/bin" ]]
           then
-            ln -s ${cfg.packages.v3}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v3}/bin/osmosisd /root/.osmosisd/cosmovisor/v3/bin
           fi
 
           echo "Symlinking osmosis v4 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v4/bin" && ! -L "/root/.osmosisd/cosmovisor/v4/bin" ]]
           then
-            ln -s ${cfg.packages.v4}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v4}/bin/osmosisd /root/.osmosisd/cosmovisor/v4/bin
           fi
 
           echo "Symlinking osmosis v5 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v5/bin" && ! -L "/root/.osmosisd/cosmovisor/v5/bin" ]]
           then
-            ln -s ${cfg.packages.v5}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v5}/bin/osmosisd /root/.osmosisd/cosmovisor/v5/bin
           fi
 
           echo "Symlinking osmosis v6 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v6/bin" && ! -L "/root/.osmosisd/cosmovisor/v6/bin" ]]
           then
-            ln -s ${cfg.packages.v6}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v6}/bin/osmosisd /root/.osmosisd/cosmovisor/v6/bin
           fi
 
           echo "Symlinking osmosis v7 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v7/bin" && ! -L "/root/.osmosisd/cosmovisor/v7/bin" ]]
           then
-            ln -s ${cfg.packages.v7}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
-          fi
-
-          echo "Symlinking osmosis v7 binary"
-          if [[ ! -f "/root/.osmosisd/cosmovisor/v7/bin" && ! -L "/root/.osmosisd/cosmovisor/v7/bin" ]]
-          then
-            ln -s ${cfg.packages.v7}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v7}/bin/osmosisd /root/.osmosisd/cosmovisor/v7/bin
           fi
 
           echo "Symlinking osmosis v8 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v8/bin" && ! -L "/root/.osmosisd/cosmovisor/v8/bin" ]]
           then
-            ln -s ${cfg.packages.v8}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v8}/bin/osmosisd /root/.osmosisd/cosmovisor/v8/bin
           fi
 
           echo "Symlinking osmosis v9 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v9/bin" && ! -L "/root/.osmosisd/cosmovisor/v9/bin" ]]
           then
-            ln -s ${cfg.packages.v9}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v9}/bin/osmosisd /root/.osmosisd/cosmovisor/v9/bin
           fi
 
           echo "Symlinking osmosis v10 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v10/bin" && ! -L "/root/.osmosisd/cosmovisor/v10/bin" ]]
           then
-            ln -s ${cfg.packages.v10}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v10}/bin/osmosisd /root/.osmosisd/cosmovisor/v10/bin
           fi
 
           echo "Symlinking osmosis v11 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v11/bin" && ! -L "/root/.osmosisd/cosmovisor/v11/bin" ]]
           then
-            ln -s ${cfg.packages.v11}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v11}/bin/osmosisd /root/.osmosisd/cosmovisor/v11/bin
           fi
 
           echo "Symlinking osmosis v12 binary"
           if [[ ! -f "/root/.osmosisd/cosmovisor/v12/bin" && ! -L "/root/.osmosisd/cosmovisor/v12/bin" ]]
           then
-            ln -s ${cfg.packages.v12}/bin/osmosisd /root/.osmosisd/cosmovisor/genesis/bin
+            ln -s ${cfg.packages.v12}/bin/osmosisd /root/.osmosisd/cosmovisor/v12/bin
           fi
         '';
         serviceConfig = {
