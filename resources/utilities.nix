5eaddaf8a1fd13e8f322a769c543459905c36324
Romain Ruetschi <romain@informal.systems>
2023-04-21 10:20:59 +0200

    Refactor `mkCosmosGoApp{Comet}` (#127)
    * Refactor common code between `mkCosmosGoApp` and `mkCosmosGoAppComet`
    
    Thanks GPT-4!
    
    * Small cleanup
    
    * Reformat code (?)
    
    * Print full build logs

--- resources/utilities.nix
+++ resources/utilities.nix
@@ -1,8 +1,8 @@
 {
   pkgs,
   nix-std,
-}: {
-  mkCosmosGoApp = {
+}: let
+  buildApp = engineRepo: args @ {
     name,
     version,
     src,
@@ -11,30 +11,29 @@
     appName ? null,
     preCheck ? null,
     ...
-  } @ args: let
+  }: let
     buildGoModuleArgs =
       pkgs.lib.filterAttrs
       (n: _:
         builtins.all (a: a != n)
         ["src" "name" "version" "vendorSha256" "appName"])
       args;
-    # gomod-json = (import ./gomod-json.nix) {inherit pkgs nix-std;};
-    # tendermint-version = gomod-json.find-tendermint-version (gomod-json.make-gomod-json { inherit name version src; } );
-    tendermint-version = with nix-std.lib; let
-      all-tm-matches =
+
+    dependency-version = with nix-std.lib; let
+      all-dep-matches =
         regex.allMatches
-        "tendermint/tendermint[[:space:]](=>[[:space:]]?[[:graph:]]*[[:space:]])?v?[[:graph:]]*"
+        "${engineRepo}[[:space:]](=>[[:space:]]?[[:graph:]]*[[:space:]])?v?[[:graph:]]*"
         (builtins.readFile "${src}/go.mod");
-      tm-string =
-        if list.any (string.hasInfix "=>") all-tm-matches
-        then list.head (list.filter (string.hasInfix "=>") all-tm-matches)
-        else list.head all-tm-matches;
-      tm-version = optional.functor.map string.strip (optional.monad.bind tm-string (regex.lastMatch "[[:space:]](v?[[:graph:]]*)"));
+      dep-string =
+        if list.any (string.hasInfix "=>") all-dep-matches
+        then list.head (list.filter (string.hasInfix "=>") all-dep-matches)
+        else list.head all-dep-matches;
+      dep-version = optional.functor.map string.strip (optional.monad.bind dep-string (regex.lastMatch "[[:space:]](v?[[:graph:]]*)"));
     in
-      optional.match tm-version {
+      optional.match dep-version {
         nothing =
           pkgs.lib.trivial.warn
-          "Could not find a tendermint version with regex, check if the formatting of go.mod escapes the regex in cosmos.nix/resources/utilities"
+          "Could not find a ${engineRepo} version with regex, check if the formatting of go.mod escapes the regex in cosmos.nix/resources/utilities"
           null;
         just = function.id;
       };
@@ -56,69 +55,15 @@
           -X github.com/cosmos/cosmos-sdk/version.AppName=${ldFlagAppName}
           -X github.com/cosmos/cosmos-sdk/version.Version=${version}
           -X github.com/cosmos/cosmos-sdk/version.Commit=${src.rev}
-          -X github.com/tendermint/tendermint/version.TMCoreSemVer=${tendermint-version}
+          -X github.com/${engineRepo}/version.TMCoreSemVer=${dependency-version}
           ${additionalLdFlags}
         '';
       }
       // buildGoModuleArgs);
-
-  mkCosmosGoAppComet = {
-    name,
-    version,
-    src,
-    vendorSha256,
-    additionalLdFlags ? "",
-    appName ? null,
-    preCheck ? null,
-    ...
-  } @ args: let
-    buildGoModuleArgs =
-      pkgs.lib.filterAttrs
-      (n: _:
-        builtins.all (a: a != n)
-        ["src" "name" "version" "vendorSha256" "appName"])
-      args;
-    cometbft-version = with nix-std.lib; let
-      all-tm-matches =
-        regex.allMatches
-        "cometbft/cometbft[[:space:]](=>[[:space:]]?[[:graph:]]*[[:space:]])?v?[[:graph:]]*"
-        (builtins.readFile "${src}/go.mod");
-      tm-string =
-        if list.any (string.hasInfix "=>") all-tm-matches
-        then list.head (list.filter (string.hasInfix "=>") all-tm-matches)
-        else list.head all-tm-matches;
-      tm-version = optional.functor.map string.strip (optional.monad.bind tm-string (regex.lastMatch "[[:space:]](v?[[:graph:]]*)"));
-    in
-      optional.match tm-version {
-        nothing =
-          pkgs.lib.trivial.warn
-          "Could not find a cometbft version with regex, check if the formatting of go.mod escapes the regex in cosmos.nix/resources/utilities"
-          null;
-        just = function.id;
-      };
-
-    ldFlagAppName =
-      if appName == null
-      then "${name}d"
-      else appName;
-  in
-    pkgs.buildGo119Module ({
-        inherit version src vendorSha256;
-        pname = name;
-        preCheck =
-          if preCheck == null
-          then ''export HOME="$(mktemp -d)"''
-          else preCheck;
-        ldflags = ''
-          -X github.com/cosmos/cosmos-sdk/version.Name=${name}
-          -X github.com/cosmos/cosmos-sdk/version.AppName=${ldFlagAppName}
-          -X github.com/cosmos/cosmos-sdk/version.Version=${version}
-          -X github.com/cosmos/cosmos-sdk/version.Commit=${src.rev}
-          -X github.com/cometbft/cometbft/version.TMCoreSemVer=${cometbft-version}
-          ${additionalLdFlags}
-        '';
-      }
-      // buildGoModuleArgs);
+in {
+  mkCosmosGoApp = buildApp "tendermint/tendermint";
+  mkCosmosGoTendermint = buildApp "tendermint/tendermint";
+  mkCosmosGoAppComet = buildApp "cometbft/cometbft";
 
   wasmdPreFixupPhase = binName: ''
     old_rpath=$(${pkgs.patchelf}/bin/patchelf --print-rpath $out/bin/${binName})
