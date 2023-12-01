3e089b2fe0dd03c06422572cd0ce501779799786
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-01 15:49:12 +0000

    fixed version

--- packages/neutron.nix
+++ packages/neutron.nix
@@ -6,7 +6,7 @@
 cosmosLib.mkCosmosGoApp {
   goVersion = "1.20";
   name = "neutron";
-  version = "v1.2.0";
+  version = "v2.0.0";
   src = neutron-src;
   vendorHash = "sha256-uLInKbuL886cfXCyQvIDZJHUC8AK9fR39yNBHDO+Qzc=";
   engine = "cometbft/cometbft";
