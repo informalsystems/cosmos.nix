5de5c01db6b7782172c27c5fbbdd44219edd6498
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-12-30 16:51:43 -0500

    checkpoint

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,8 +1,8 @@
 {inputs, ...}: {
   flake.packages."x86_64-linux" = {
       aarch64-linux-vm =
-        let pkgs-x86_64 = import inputs.nixpkgs { system = "x86_64-linux"; };
-            pkgs-aarch64 = import inputs.nixpkgs { system = "aarch64-linux"; };
+        let pkgs-x86_64 = inputs.nixpkgs.legacyPackages."x86_64-linux";
+            pkgs-aarch64 = inputs.nixpkgs.legacyPackages."aarch64-linux";
             drive-flags = "format=raw,readonly=on";
         in pkgs-x86_64.writeScriptBin "run-nixos-vm-aarch64" ''
 
@@ -10,7 +10,7 @@
         ${pkgs-x86_64.qemu_full}/bin/qemu-system-aarch64 \
         -machine virt \
         -cpu cortex-a57 \
-        -m 2G \
+        -m 40G \
         -nographic \
         -drive if=pflash,file=${pkgs-aarch64.OVMF.fd}/AAVMF/QEMU_EFI-pflash.raw,${drive-flags} \
         -drive file=${inputs.self.packages."x86_64-linux".aarch64-linux-iso}/iso/nixos.iso,${drive-flags}
@@ -33,8 +33,21 @@
                 extraGroups = [ "wheel" ];
                 initialPassword = "test";
               };
+  
+              nix = {
+                package = pkgs.nixFlakes;
+                settings = {
+                  experimental-features = ["nix-command" "flakes"];
+                };
+              };
 
-              services.getty.autologinUser = "test";
+              environment.systemPackages = with pkgs; [
+                vim
+                htop
+                postgresql_16
+              ];
+
+              services.getty.autologinUser = "root";
             };
         in inputs.nixos-generators.nixosGenerate {
           system = "x86_64-linux";
