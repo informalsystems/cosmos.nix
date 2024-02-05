6a50c0c95302de7c486d6fd7d75b944f5ac76a90
Jonathan Lorimer <jonathan_lorimer@mac.com>
2024-02-05 11:40:26 -0500

    format

--- packages/hermes.nix
+++ packages/hermes.nix
@@ -7,12 +7,12 @@ pkgs.rustPlatform.buildRustPackage {
   version = "v1.7.4";
   src = hermes-src;
   nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default];
-  buildInputs = with pkgs; 
-    lib.lists.optionals stdenv.isDarwin 
-      [
-        darwin.apple_sdk.frameworks.Security
-        darwin.apple_sdk.frameworks.SystemConfiguration
-      ];
+  buildInputs = with pkgs;
+    lib.lists.optionals stdenv.isDarwin
+    [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
   cargoSha256 = "sha256-oAsRn0THb5FU1HqgpB60jChGeQZdbrPoPfzTbyt3ozM=";
   doCheck = false;
 }
