0356d009446f3a45821e269a229d0aefb607e386
Jonathan Lorimer <32466011+JonathanLorimer@users.noreply.github.com>
2024-02-06 10:12:45 -0500

    Add CI support for Mac (#212)
    * test mac matrix
    
    * fix beaker on mac
    
    * fix beaker on mac 2
    
    * fmt
    
    * try and fix beaker on mac
    
    * debug apalache
    
    * only build apalache on mac
    
    * fix syntax errors
    
    * remove apalache check
    
    * add mac build inputs to hermes
    
    * format
    
    * only build stargaze on mac
    
    * test namada
    
    * test namada
    
    * test namada: add libusb hidapi buildinputs
    
    * move namada to linux only
    
    * remove namada check from ci
    
    * separate linux app packages
    
    * fix emoji

--- packages/hermes.nix
+++ packages/hermes.nix
@@ -6,7 +6,13 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "hermes";
   version = "v1.7.4";
   src = hermes-src;
-  nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default] ++ lib.lists.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
+  nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default];
+  buildInputs = with pkgs;
+    lib.lists.optionals stdenv.isDarwin
+    [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
   cargoSha256 = "sha256-oAsRn0THb5FU1HqgpB60jChGeQZdbrPoPfzTbyt3ozM=";
   doCheck = false;
 }
