45484c158c22e1bf960988a3c7e8dfedbcaaa096
Romain Ruetschi <romain@informal.systems>
2024-03-13 21:49:19 +0100

    Update packages/rollapp-evm.nix
    Co-authored-by: Jonathan Lorimer <32466011+JonathanLorimer@users.noreply.github.com>

--- packages/rollapp-evm.nix
+++ packages/rollapp-evm.nix
@@ -5,7 +5,7 @@
 mkCosmosGoApp {
   name = "rollapp-evm";
   version = "v2.0.0-beta-7-g21b29f6";
-  src = "${rollapp-evm-src}";
+  src = rollapp-evm-src;
   rev = rollapp-evm-src.rev;
   vendorHash = "sha256-rg3ZrNIMuUUW01lyjklTxn4zYlOiwFXyTqSE7scaRAk=";
   tags = ["netgo"];
