19a5801793553056eb1e791645e418d879d9d519
Jonathan Lorimer <32466011+JonathanLorimer@users.noreply.github.com>
2024-03-20 12:54:44 -0400

    Update nix-std to get toTOML that omits nulls (#234)
    * update nix-std, add some comments
    
    * format

--- nixosModules/hermes/base.nix
+++ nixosModules/hermes/base.nix
@@ -1,3 +1,16 @@
+# This module might be slightly confusing, since nixos modules are isntantiated via a "fixed point"
+# which is effectively a loop that accumulates attributes.
+#
+# because of this "fixed point" and lazyness we are able to set the "options"
+# attribute, which declares the schema for the module and is required at the
+# beginning of the loop, and the "config" attribute which is a serialization of
+# the accumulated configuration into a toml file and intuitively happens at the
+# end of the loop
+#
+# TL;DR:
+# - 'options' represents the schema for the hermes configuration module
+# - 'config.toml' represents the a final serialization of the actual configuration into a toml file
+# - for more information on nixos modules see https://nix.dev/tutorials/module-system/module-system.html
 {
   lib,
   nix-std,
@@ -9,10 +22,6 @@ with lib; {
     toml = mkOption {type = types.unique {message = "only one toml output";} types.str;};
   };
   config = {
-    toml = let
-      # remove `null` from toml render
-      sanitizedCfg = filterAttrsRecursive (_: v: v != null) cfg.config;
-    in
-      nix-std.lib.serde.toTOML sanitizedCfg;
+    toml = nix-std.lib.serde.toTOML cfg.config;
   };
 }
