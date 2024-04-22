47a6d6d4d40d9770b23147951635bef0ac5507c5
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2024-04-22 06:27:34 +0200

    Update Migaloo chain to v4.1.3 (#248)

--- packages/migaloo.nix
+++ packages/migaloo.nix
@@ -1,17 +1,17 @@
 {
   migaloo-src,
   cosmosLib,
-  libwasmvm_1_2_3,
+  libwasmvm_1_5_2,
 }:
 cosmosLib.mkCosmosGoApp {
   name = "migaloo";
-  version = "v3.0.2";
+  version = "v4.1.3";
   src = migaloo-src;
   rev = migaloo-src.rev;
-  vendorHash = "sha256-GQDfI4hSkkrsBfIczdGoOhghR7/FqEvXavyP4E6iHM4=";
-  engine = "tendermint/tendermint";
+  vendorHash = "sha256-2rQm+pVniubKXkH3rXlQUOtgXm2Vp0faaqvU7QpEXN4=";
+  engine = "cometbft/cometbft";
   preFixup = ''
-    ${cosmosLib.wasmdPreFixupPhase libwasmvm_1_2_3 "migalood"}
+    ${cosmosLib.wasmdPreFixupPhase libwasmvm_1_5_2 "migalood"}
   '';
-  buildInputs = [libwasmvm_1_2_3];
+  buildInputs = [libwasmvm_1_5_2];
 }
