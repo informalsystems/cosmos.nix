fe4703125eafe579d01de42537234af39e823064
Luca Joss <luca@informal.systems>
2024-09-30 14:32:08 +0200

    Update Juno from v24.0.0 to v25.0.0

--- packages/juno.nix
+++ packages/juno.nix
@@ -1,21 +1,21 @@
 {
   cosmosLib,
   juno-src,
-  libwasmvm_1_5_4,
+  libwasmvm_1_5_5,
 }:
 cosmosLib.mkCosmosGoApp {
   name = "juno";
-  version = "v24.0.0";
+  version = "v25.0.0";
   goVersion = "1.22";
   src = juno-src;
   rev = juno-src.rev;
-  vendorHash = "sha256-srBwnVyOx6Zt6n2e6WhZd+uHWNnpyv6fQTi1A9jsVd0=";
+  vendorHash = "sha256-HDHsBuuJ+ta3ynYv8NVqEdd0h4UNWBelUA8j+YoEf4E=";
   tags = ["netgo"];
   engine = "cometbft/cometbft";
   excludedPackages = ["interchaintest"];
   preFixup = ''
-    ${cosmosLib.wasmdPreFixupPhase libwasmvm_1_5_4 "junod"}
+    ${cosmosLib.wasmdPreFixupPhase libwasmvm_1_5_5 "junod"}
   '';
   dontStrip = true;
-  buildInputs = [libwasmvm_1_5_4];
+  buildInputs = [libwasmvm_1_5_5];
 }
